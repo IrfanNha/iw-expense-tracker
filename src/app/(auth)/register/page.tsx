4c885c52e7d1eaf6415fc2f2dd053d04068b1271
IrfanNha <mazirfan0099@gmail.com>
2026-07-07 12:36:32 +0700

    Fix Flag Feature

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -105,7 +105,7 @@ export default function RegisterPage() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 
           ...data, 
-          ...(isDevelopment ? {} : { turnstileToken }) 
+          ...(isTurnstileEnabled ? { turnstileToken } : {}) 
         }),
       });
 
